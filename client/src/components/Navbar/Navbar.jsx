1eed910218320a75aa5ae6eefacf72580824c0a6
Sahil Khatri <Sahil Khatri>
2025-01-28 21:12:51 +0530

    Worked on header section and view category for home page

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css";
 import { assets } from "../../assets/frontend_assets/assets";
 
 const Navbar = () => {
-  const [menu, setMenu] = useState("menu");
+  const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
