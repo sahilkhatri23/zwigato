c54fa6de1eec9ce499c5fb146fe791d6d7f0e93f
Sahil Khatri <Sahil Khatri>
2025-02-01 15:04:03 +0530

    Add to cart context api integration and filter according to category

--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import "./FoodDisplay.css";
 import { useContext } from "react";
-import { StoreContext } from "../../context/StoreContext.jsx";
-import FoodItem from "../FoodItem/FoodItem.jsx";
+import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
 const FoodDisplay = ({ category }) => {
-  const food_list = useContext(StoreContext).food_list;
+  const { food_list } = useContext(StoreContext);
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((food, index) => {
-          return <FoodItem key={index} {...food} />;
+          if (category === "All" || category === food.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={food._id}
+                name={food.name}
+                price={food.price}
+                description={food.description}
+                image={food.image}
+              />
+            );
+          }
         })}
       </div>
     </div>
