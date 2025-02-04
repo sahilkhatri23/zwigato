4b415e46b93bda5ae0c817e2f02a28cc9ada7f4b
Sahil Khatri <Sahil Khatri>
2025-02-05 01:19:19 +0530

    added modal for login signup

---
+++ client/src/components/LoginPopUp/LoginPopUp.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./LoginPopUp.css";
+import { assets } from "../../assets/frontend_assets/assets";
+const LoginPopUp = ({ setShowLogin }) => {
+  const [currentState, setCurrentState] = useState("Sign Up");
+  return (
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currentState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
+        </div>
+        <div className="login-popup-inputs">
+          {currentState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your Name" required />
+          )}
+          <input type="email" placeholder="Your Email" required />
+          <input type="password" placeholder="Password" required />
+        </div>
+        <button>
+          {currentState === "Sign Up" ? "Create account" : "Login"}
+        </button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, I agree to the terms of use & Privacy Policy. </p>
+        </div>
+        {currentState === "Login" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrentState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrentState("Login")}>Login here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopUp;
